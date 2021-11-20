a165b5806443bf891aa025859629737d11e405c1
ShavarshP <63345390+ShavarshP@users.noreply.github.com>
2021-11-20 19:08:00 +0400

    Update index.js
    delete: commit

--- src/components/cateCard/index.js
+++ src/components/cateCard/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { NavLink } from "react-router-dom";
+
 
 const CatsCard = ({ cat }) => {
   return (
@@ -7,20 +7,6 @@ const CatsCard = ({ cat }) => {
       <div className="product-img">
         <img src={cat.url} />
       </div>
-      {/* <div className="product-list">
-        <h3>{"props.state.street"}</h3>
-        <div className="actions">
-          <div className="add-to-links"> */}
-      {/* <NavLink to={"/myhome/" + props.state._id} className="cart-button">
-              {props.state.rooms}.room
-            </NavLink>
-            <NavLink
-              to={"/home/" + props.state._id}
-              className="compare"
-            ></NavLink> */}
-      {/* </div>
-        </div>
-      </div> */}
     </div>
   );
 };
